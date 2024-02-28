3c62c7444b3fe65093012709cbd32a80ffb08f2e
VecomeMKS <revy3d@gmail.com>
2024-02-28 15:41:25 +0200

    InitTestProject

---
+++ PokeDraft/PokeDraft.Tests/Helpers/DBContextHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokeDraft.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeDraft.Tests.Helpers
+{
+    public class DBContextHelper
+    {
+        public static ApplicationDBContext GetDBContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                .Options;
+            var databaseContext = new ApplicationDBContext(options);
+            databaseContext.Database.EnsureCreated();
+            return databaseContext;    
+        }
+    }
+}
---
+++ PokeDraft/PokeDraft.Tests/Services/TypesServiceTest.cs
@@ -0,0 +1,55 @@
+﻿using PokeDraft.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokeDraft.Services.TypesServices;
+using Type = PokeDraft.Models.Type;
+using FluentAssertions;
+using PokeDraft.Exceptions;
+using PokeDraft.Exceptions.Type;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+
+namespace PokeDraft.Tests.Controller
+{
+    public class TypesServiceTest
+    {
+        private readonly ApplicationDBContext _contextInMemory;
+        private readonly TypesService _typesService;
+
+        public TypesServiceTest()
+        {
+            _contextInMemory = Helpers.DBContextHelper.GetDBContext();
+            _typesService = new TypesService(_contextInMemory);
+        }
+
+        [Fact]
+        public async Task DeleteType_TypeDoesNotExist_ReturnFalseAsync()
+        {
+            Func<Task> func = async () => await _typesService.DeleteTypeAsync(new Type { TypeName = "Mud"});
+            Func<Task> func2 = async () => await _typesService.DeleteTypeAsync(new Type { TypeName = "Water" });
+
+            await func.Should().ThrowAsync<TypeDoesNotExistException>();
+            func2.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetTypes_TypesAreNotNull_ReturnFalseAsync()
+        {
+            var result = await _typesService.GetTypesAsync();
+
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetTypeById_TypeExists_ReturnType()
+        {
+            var result = await _typesService.GetTypeByIdAsync("Steel");
+            result.Should().NotBeNull();
+        }
+
+
+    }
+}
